884ced544bb4f526428e9a072195278d4d122664
yndben <yndben50@gmail.com>
2026-03-05 10:40:43 +0400

    feat: ajout fonction restaurer_base

--- dataTask.sh
+++ dataTask.sh
@@ -8,6 +8,7 @@ initialiser_base(){
       echo "La base a ete cree avec succes"
   fi
 }
+
 afficher_toutes_taches(){
   if [ -f "tasks.txt" ]; then
       cat tasks.txt
@@ -15,11 +16,21 @@ afficher_toutes_taches(){
       echo "Erreur: Base non initialise"
   fi
 }
+
 sauvegarder_base(){
   if [ -f "tasks.txt" ]; then 
       cp tasks.txt tasks_backup.txt 
       echo "Sauvegarde cree: tasks_backup.txt"
 
+  fi
+}
+
+restaurer_base(){
+  if [ -f "tasks_backup.txt" ]; then 
+       cp tasks_backup.txt tasks.txt
+       echo "base restauree depuis la sauvegarde"
+  else 
+       echo "Aucun sauvegarde disponible"
   fi
 }
 
