0b8a3aabc5591581e6b7f31b5523fefca2998fa1
yndben <yndben50@gmail.com>
2026-03-05 10:16:19 +0400

    feat: ajout fonction initialiser_base

---
+++ dataTask.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+initialiser_base(){
+
+  if [ -f "tasks.txt" ]; then 
+      echo "La base existe deja"
+  else 
+      touch tasks.txt
+      echo "La base a ete cree avec succes"
+  fi
+}
+
